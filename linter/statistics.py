0a3712d73956cc9f2823667dd8a50723555c0bb5
3top1a <3top1a.official@gmail.com>
2023-09-06 19:58:38 +0200

    Added statistics output file argument

--- linter/statistics.py
+++ linter/statistics.py
@@ -6,6 +6,7 @@ import datetime
 import json
 import logging
 import os
+import sys
 
 import lib
 import psycopg2
@@ -21,8 +22,13 @@ def main():
     root_logger.removeHandler(root_logger.handlers[0])
     root_logger.addHandler(console_handler)
 
-    if not os.path.exists("data.json"):
-        with open("data.json", "w") as file:
+    if len(sys.argv) != 2:
+        logging.critical("Please specify output file")
+        return
+    output_file = sys.argv[1]
+
+    if not os.path.exists(output_file):
+        with open(output_file, "w") as file:
             file.write('{"data": []}')
 
     time = int(datetime.datetime.now(tz=datetime.timezone.utc).timestamp())
@@ -82,7 +88,7 @@ def main():
         EDAM_INVALID,
     )
 
-    with open("data.json") as json_file:
+    with open(output_file) as json_file:
         data = json.load(json_file)
 
     new_data_entry = {
@@ -90,23 +96,25 @@ def main():
         "total_count_on_biotools": total_count_on_biotools,
         "total_errors": total_errors,
         "unique_tools": unique_tools,
-        "URL_INVALID": URL_INVALID,
-        "URL_PERMANENT_REDIRECT": URL_PERMANENT_REDIRECT,
-        "URL_BAD_STATUS": URL_BAD_STATUS,
-        "URL_NO_SSL": URL_NO_SSL,
-        "URL_UNUSED_SSL": URL_UNUSED_SSL,
-        "URL_TIMEOUT": URL_TIMEOUT,
-        "URL_SSL_ERROR": URL_SSL_ERROR,
-        "URL_CONN_ERROR": URL_CONN_ERROR,
-        "URL_LINTER_ERROR": URL_LINTER_ERROR,
-        "EDAM_OBSOLETE": EDAM_OBSOLETE,
-        "EDAM_NOT_RECOMMENDED": EDAM_NOT_RECOMMENDED,
-        "EDAM_INVALID": EDAM_INVALID,
+        "error_types": {
+            "URL_INVALID": URL_INVALID,
+            "URL_PERMANENT_REDIRECT": URL_PERMANENT_REDIRECT,
+            "URL_BAD_STATUS": URL_BAD_STATUS,
+            "URL_NO_SSL": URL_NO_SSL,
+            "URL_UNUSED_SSL": URL_UNUSED_SSL,
+            "URL_TIMEOUT": URL_TIMEOUT,
+            "URL_SSL_ERROR": URL_SSL_ERROR,
+            "URL_CONN_ERROR": URL_CONN_ERROR,
+            "URL_LINTER_ERROR": URL_LINTER_ERROR,
+            "EDAM_OBSOLETE": EDAM_OBSOLETE,
+            "EDAM_NOT_RECOMMENDED": EDAM_NOT_RECOMMENDED,
+            "EDAM_INVALID": EDAM_INVALID,
+        },
     }
     data["data"].append(new_data_entry)
 
     # Step 3: Save the updated data back to the JSON file
-    with open("data.json", "w") as json_file:
+    with open(output_file, "w") as json_file:
         json.dump(data, json_file, indent=2)
 
     logging.info("Appended to the json file")
